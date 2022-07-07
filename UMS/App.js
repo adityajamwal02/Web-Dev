562e4f9aa48d2139e2bf3fbebdba0c7af04ff8b1
ADITYA JAMWAL <aditya.vicky01@gmail.com>
2022-07-08 01:29:00 +0530

    Add files via upload

---
+++ UMS/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import HomePage from './components/pages/HomePage';
+import AboutPage from './components/pages/AboutPage';
+import ContactPage from './components/pages/ContactPage';
+import NavBar from './components/pages/layout/NavBar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import PageNotFound from './components/pages/PageNotFound';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route exact path="/home" element={<HomePage />} />
+          <Route exact path="/about" element={<AboutPage />} />
+          <Route exact path="/contact" element={<ContactPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
